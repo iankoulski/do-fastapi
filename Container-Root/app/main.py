9b1bf74e110cb68a9e8ef7a8bd429fcc2f4e6663
iankoulski <alex_iankoulski@yahoo.com>
2025-01-15 09:18:52 +0000

    Enable run on kubernetes

--- Container-Root/app/main.py
+++ Container-Root/app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 import logging
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 logger = logging.getLogger(__name__)
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
